81433a683d9cd9eb349f0d41a6a2896ff5e5c8f1
filipeyuan <fyuansantos23@gmail.com>
2026-07-20 20:40:08 -0300

    refactor: adiciona declare(strict_types=1) em todo o código PHP (padrão da empresa)

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use App\Http\Controllers\Api\AppointmentController;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\ServiceController;
